c94acd3f2b7899e25e860150dbb8608485806769
Brian Bourn <ba.bourn@gmail.com>
2020-12-27 16:06:07 -0800

    Simple Camera Streamer and Writer
    Signed-off-by: babourn <ba.bourn@gmail.com>

---
+++ main.py
@@ -0,0 +1,73 @@
+
+import cv2
+import time
+import os
+import argparse
+import threading
+from imutils.video.videostream import  VideoStream
+from utils import server
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-o", "--output", type=str,
+                    help="path to optional output video file")
+    ap.add_argument("-i", "--input", type=str,
+                    help="path to optional output video file")
+    ap.add_argument("-u", "--use-pi-camera",
+                    help="use an rpi camera")
+    ap.add_argument("-b", "--bottle-server", action="store_true",
+                    help="enable a local bottle server to stream images")
+    ap.add_argument("-r", "--resolution", type=tuple, default=(320, 480),
+                    help="Set stream resolution")
+    args = vars(ap.parse_args())
+
+    if args["bottle_server"] is not None:
+        t = threading.Thread(target=server.serve, args=())
+        t.daemon = True
+        t.start()
+
+    writer = None
+    vs = None
+
+    if args["input"] is None:
+        print("[INFO] starting video stream...")
+        # vs = VideoStream(src=0).start()
+        vs = VideoStream(usePiCamera=args["use_pi_camera"],
+                         resolution=args["resolution"])
+        vs.start()
+        time.sleep(2.0)
+
+    # otherwise, grab a reference to the video file
+    else:
+        print("[INFO] opening video file...")
+        vs = cv2.VideoCapture(args["input"])
+
+    while True:
+        frame = vs.read()
+        frame = frame[1] if args["input"] is not None else frame
+
+        # if we are viewing a video and we did not grab a frame then we
+        # have reached the end of the video
+        if args["input"] is not None and frame is None:
+            break
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        if writer is None and args["output"] is not None:
+            output_path = os.path.join(args["output"], "vanilla.avi")
+            (H, W) = frame.shape[:2]
+            fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+            writer = cv2.VideoWriter(output_path, fourcc, 30, (W, H), True)
+
+        if writer is not None:
+            writer.write(frame)
+
+        if args["bottle_server"] is not None:
+            with server.lock:
+                server.outputFrame = frame
+
+    print("Shutting Down")
+    vs.close()
+    if writer is not None:
+        writer.release()
---
+++ utils/server.py
@@ -0,0 +1,37 @@
+from bottle import template, static_file, response, Bottle
+import threading
+import imutils
+import time
+import cv2
+
+outputFrame = None
+lock = threading.Lock()
+app = Bottle()
+
+@app.route("/")
+def index():
+  return template('utils/views/index', feed=app.get_url('/feed'))
+
+@app.route("/body.css")
+def body():
+  return static_file('body.css', root='./utils/views')
+
+@app.route("/feed")
+def feed():
+  response.content_type = "multipart/x-mixed-replace; boundary=frame"
+  return generate()
+
+def generate():
+  global outputFrame, lock
+
+  while True:
+    with lock:
+      if outputFrame is None:
+        continue
+      (flag, encodedImage) = cv2.imencode(".jpg", cv2.cvtColor(outputFrame, cv2.COLOR_BGR2RGB))
+      if not flag:
+        continue
+    yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+
+def serve():
+  app.run(host='0.0.0.0', port='80', server='waitress')
